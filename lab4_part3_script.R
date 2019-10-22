d0737520cdd3fc36d6476ca74f1a469504a5aa19
mwewells <mwells@ucsb.edu>
2019-10-22 14:00:55 -0700

    adsf

---
+++ lab4_part3_script.R
@@ -0,0 +1,22 @@
+# Michael Wells
+
+library(tidyverse)
+library(here)
+library(janitor)
+
+
+db <- read_csv(here::here("data","disease_burden.csv")) %>% 
+  clean_names() %>% 
+  rename(deaths_per_100k = death_rate_per_100_000)
+
+db_sub <- db %>% 
+  filter(country_name %in% c("United States","Japan","Afganistan","Somalia")) %>% 
+  filter(age_group == "0-6 days", sex == "Both")
+
+#Graph
+
+ggplot(db_sub, aes(x = year, y = deaths_per_100k))+
+  geom_line(aes(color = country_name))
+
+ggsave(here::here("final graphs","disease_graph.png"))
+
